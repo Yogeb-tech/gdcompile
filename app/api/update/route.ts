77dc8a9e376b5a05cee7d89545e0b36cb700935f
Flavore669 <dirkosh2@gmail.com>
2026-06-13 01:25:31 -0500

    basic implementation for github webhook

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -3,7 +3,27 @@
 import { createClient } from '@supabase/supabase-js';
 import { StatusCodes } from 'http-status-codes';
 import { NextResponse } from 'next/server';
-
+interface GitHubWebhookPayload {
+	action: string;
+	workflow_job: {
+		id: number;
+		run_id: number;
+		workflow_name: string;
+		head_branch: string;
+		run_url: string;
+		run_attempt: number;
+		node_id: string;
+		head_sha: string;
+		url: string;
+		html_url: string;
+		status: string;
+		conclusion: null | string;
+		created_at: string;
+		started_at: string;
+		completed_at: null | string;
+		name: string;
+	};
+}
 const supabase = createClient(
 	process.env.NEXT_PUBLIC_SUPABASE_URL!,
 	process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
@@ -11,7 +31,14 @@ const supabase = createClient(
 
 export async function POST(request: Request) {
 	try {
-		
+		const body = await request.json();
+		const githubPayload = body as GitHubWebhookPayload;
+		console.log('Request body:', githubPayload);
+		console.log('Request headers:', Object.fromEntries(request.headers.entries()));
+		return NextResponse.json(
+			{ message: 'Webhook Received' },
+			{ status: StatusCodes.INTERNAL_SERVER_ERROR }
+		);
 	} catch (error) {
 		console.error('Update error:', error);
 		return NextResponse.json(
