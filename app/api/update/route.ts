8ff15b900f42330a2a1026f66928270b74ec9ff4
Flavore669 <dirkosh2@gmail.com>
2026-06-13 02:36:18 -0500

    update endpoint works

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -1,5 +1,9 @@
 // TODO: Create update post request to update status from github webhooks
-
+// TODO: Adjust routing to be more RESTful
+//		 /workflows/{workflow_id} - GET or DELETE a workflow
+// 		 /workflows - POST: create a new workflow
+// TODO: After this add delete endpoint
+// TODO: Then configure RLS in supabase
 import { createClient } from '@supabase/supabase-js';
 import { StatusCodes } from 'http-status-codes';
 import { NextResponse } from 'next/server';
@@ -35,8 +39,26 @@ export async function POST(request: Request) {
 	try {
 		const body = await request.json();
 		const githubPayload = body as GitHubWebhookPayload;
+
 		console.log('Request body:', githubPayload);
 		console.log('Request headers:', Object.fromEntries(request.headers.entries()));
+
+		const { error } = await supabase
+			.from('jobs')
+			.update({
+				status: githubPayload.workflow_job.status,
+				completed_at: githubPayload.workflow_job.completed_at,
+			})
+			.eq('id', Number(githubPayload.workflow_job.run_id));
+
+		if (error) {
+			console.error('supabase update error:', error);
+			return NextResponse.json({
+				error: `Failed to alter job: ${githubPayload.workflow_job.id}`,
+				status: StatusCodes.INTERNAL_SERVER_ERROR,
+			});
+		}
+
 		return NextResponse.json({ message: 'Webhook Received' }, { status: StatusCodes.OK });
 	} catch (error) {
 		console.error('Update error:', error);
