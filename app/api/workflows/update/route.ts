fe5e300efe2379e10d81e51ce74680705cd7eb07
Flavore669 <dirkosh2@gmail.com>
2026-06-23 21:09:36 -0500

    minor change

--- app/api/workflows/update/route.ts
+++ app/api/workflows/update/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 import { Webhooks } from '@octokit/webhooks';
 
 const webhooks = new Webhooks({
-	secret: process.env.WEBHOOK_SECRET!,
+	secret: process.env.GITHUB_WEBHOOK_SECRET!,
 });
 
 interface GitHubWorkflowRunPayload {
